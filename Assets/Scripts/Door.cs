2b19d6520256313c91491cd79399ca65b603c516
dddd37 <dddd45690@gmail.com>
2023-12-26 21:29:36 +0300

    [new] animated doors

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -11,39 +11,49 @@ public class Door : MonoBehaviour
     public bool doorForAir = true;
     public UnityEvent CharacterGoIn = new UnityEvent();
     public UnityEvent CharacterGoOut = new UnityEvent();
+    public Animator animator;
+
+
     private void Awake()
     {
-        sp=GetComponent<SpriteRenderer>();
+        sp = GetComponent<SpriteRenderer>();
         sp.sprite = closedDoor;
+        animator = GetComponent<Animator>();
     }
+
     private void Start()
     {
         if (doorForAir) sp.color = Color.cyan;
         else sp.color = Color.magenta;
+        sp.sortingOrder = 0;
     }
+
     public void onTogglePressed()
     {
         sp.sprite = openedDoor;
         isOpen = true;
+        animator.SetBool("OPEN", true);
     }
+
     public void onToggleReleased()
     {
         sp.sprite = closedDoor;
         isOpen = false;
+        animator.SetBool("OPEN", false);
     }
+
     private void OnTriggerEnter2D(Collider2D other)
-    {        
-        if (isOpen && (other.tag == "AirCharacter" && doorForAir || other.tag == "EarthCharacter" && !doorForAir)) 
+    {
+        if (isOpen && ((other.tag == "AirCharacter" && doorForAir) || (other.tag == "EarthCharacter" && !doorForAir)))
         {
-            
             CharacterGoIn.Invoke();
         }
     }
+
     private void OnTriggerExit(Collider other)
     {
-        if (isOpen && (other.tag == "AirCharacter" && doorForAir || other.tag == "EarthCharacter" && !doorForAir))
+        if (isOpen && ((other.tag == "AirCharacter" && doorForAir) || (other.tag == "EarthCharacter" && !doorForAir)))
         {
-
             CharacterGoOut.Invoke();
         }
     }
